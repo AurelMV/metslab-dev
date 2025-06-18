9678e0e401b468e024eebb9b3777426d13a7743d
Marco Valdez <marcotuamortlv@gmail.com>
2025-06-18 13:41:52 -0500

    fix: cors

--- backend-api/config/cors.php
+++ backend-api/config/cors.php
@@ -2,7 +2,13 @@
 return [
     'paths' => ['api/*', 'storage/*', 'auth/*', 'sanctum/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://www.metslab3d.com',
+        'https://metslab3d.com',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'app://.',
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
